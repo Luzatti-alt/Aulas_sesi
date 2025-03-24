ce5d6f862fb8a2fcf389cb5720fffcade185002e
Luzatti-alt <lucasguzatti4@gmail.com>
2025-03-24 10:56:03 -0300

    Update Iden.java

--- Iden.java
+++ Iden.java
@@ -1,14 +1,2 @@
-package java_aulas;
-
-public class Iden {
-	public static void main(String[] args){
-		String nome ="A blu blé";
-		int idade = 69;
-		String cidade ="is goto";
-		
-		System.out.println("nome: "+nome);
-		System.out.println("idade: "+idade);
-		System.out.println("cidade: "+cidade);
-		
-	}
-}
+aaa
+	
