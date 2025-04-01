f76ed4c948d0b3efa6d717846f33dccb3f11bf1d
Luzatti-alt <lucasguzatti4@gmail.com>
2025-04-01 11:11:24 -0300

    Add files via upload

---
+++ par_impar.java
@@ -0,0 +1,10 @@
+package java_aulas;
+import java.util.Scanner;
+public class par_impar {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("digite um número: ");
+		int n1 = scanner.nextInt();
+		int res = (n1 % 2);
+		system.out.println(res);
+}}
---
+++ tabuada.java
@@ -0,0 +1,14 @@
+package java_aulas;
+import java.util.Scanner;
+public class tabuada {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("digite um número: ");
+		int n1 = scanner.nextInt();
+		int i = 0;
+		while (i != 101) {
+			System.out.println(i+"x"+n1+"="+i*n1);
+			i++;
+		}
+	}
+}
