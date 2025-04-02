df25e5a286ac1e6f81e5812e48e8734d9867e8f2
Luzatti-alt <lucasguzatti4@gmail.com>
2025-04-02 06:59:48 -0300

    Add files via upload

---
+++ gamble.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class gamble {
+    public static void main(String[]args){
+        double fichas = 1000;
+        Scanner ler = new Scanner(System.in);
+        while(fichas != 0){
+            System.out.print("quantos você apostaria: ");
+            double aposta = ler.nextDouble();
+            System.out.println("quantidade de fichas: "+fichas);
+            System.out.println("apostou: "+ aposta);
+            System.out.println("gostaria de apostar em qual número(0-100): ");
+            double num = ler.nextInt();
+            double sorte = Math.floor(Math.random()*100);
+            System.out.println("o número sorteado é: "+sorte+" \n");
+            if (sorte == num){
+                fichas += fichas * 0.03;
+            }else{fichas -= aposta;}
+        }System.out.println("fim do jogo,você faliu.");
+    }
+}
