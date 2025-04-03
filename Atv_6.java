e5d4e36ba76b1d7861ed32ed37bf9a8c93becfc4
Luzatti-alt <lucasguzatti4@gmail.com>
2025-04-03 11:25:27 -0300

    Update Atv_6.java

--- Atv_6.java
+++ Atv_6.java
@@ -6,8 +6,11 @@ public class Atv_6 {
 		Scanner ler = new Scanner(System.in);
 		System.out.println("Digite sua senha: ");
 		String senha = ler.nextLine();
-		if (senha.equals("1234")) {
+		if (senha.lenght()>=8) {
+			//.lenght()>=8 limita até 8 letras
 			System.out.println("acesso garantido");
+		}else{
+			System.out.println("acesso negado");
 		}
 	}
 }
