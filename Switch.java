b1e3cc2beeeb95a6712ddd6d954b3410d3042a0f
Luzatti-alt <lucasguzatti4@gmail.com>
2025-04-07 09:18:11 -0300

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,21 @@
+package java_aulas;
+import java.util.Scanner;
+public class Switch {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		System.out.println("1- Cadastrar usuario:");
+		System.out.println("2- Excluir usuario");
+		System.out.println("3- sair ");
+		int ops = ler.nextInt();
+		switch(ops) {
+		case 1:
+			System.out.println("Usuario Cadastrado");
+			break;
+		case 2:
+			System.out.println("Usuario Excluido");
+			break;
+		default:
+			System.out.println("saindo");
+		}
+	}
+}
