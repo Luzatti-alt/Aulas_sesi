76f41542bd5294923bc95cde807a74efeeaf83f6
Luzatti-alt <lucasguzatti4@gmail.com>
2025-05-14 11:42:39 -0300

    Update at4.java
    Signed-off-by: Luzatti-alt <lucasguzatti4@gmail.com>

--- Java/at4.java
+++ Java/at4.java
@@ -1,7 +1,6 @@
 package java_aulas;
 import java.util.Scanner;
 public class at4 {
-
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		System.out.println("digite um número: ");
