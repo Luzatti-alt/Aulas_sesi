2a95a08f41148c6a5f7e9aa32b5ada90e51af569
Luzatti-alt <lucasguzatti4@gmail.com>
2025-05-14 11:35:45 -0300

    Update at10.java
    Signed-off-by: Luzatti-alt <lucasguzatti4@gmail.com>

--- Java/at10.java
+++ Java/at10.java
@@ -1,11 +1,8 @@
 package java_aulas;
-
 public class at10 {
-
 	public static void main(String[] args) {
-		for(int i=0;i>11;i++) {
+		for(int i=0;i<=11;i++) {
 			System.out.println(i);
 		}
 	}
-
 }
