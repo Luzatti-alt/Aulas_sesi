3ab9fde63ff8ac2cffc719857f77becf2546e145
Luzatti-alt <lucasguzatti4@gmail.com>
2025-09-24 10:57:22 -0300

    Update script.js
    Signed-off-by: Luzatti-alt <lucasguzatti4@gmail.com>

--- Web/HTML/atvs/site_proprio/js/script.js
+++ Web/HTML/atvs/site_proprio/js/script.js
@@ -1,1 +1,8 @@
+// Obtém o botão e o elemento de áudio
+const button = document.getElementById('tocar');
+const guaranaSound = document.getElementById('guarana');
 
+// Adiciona o evento de clique no botão
+button.addEventListener('click', function() {
+    guaranaSound.play(); // Toca o som
+});
